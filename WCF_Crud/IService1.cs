ae5a2555888c98df232e87af0487e230913cddd6
gideon.c <gideon.c@ITPL-SOFT-001>
2020-01-22 17:06:38 +0530

    Create WCF

--- WCF_Crud/IService1.cs
+++ WCF_Crud/IService1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -12,36 +13,88 @@ namespace WCF_Crud
     [ServiceContract]
     public interface IService1
     {
-
         [OperationContract]
-        string GetData(int value);
-
+        string InsertEmpDetails(EmpDetails eDatils);
         [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
-        // TODO: Add your service operations here
+        DataSet GetEmpDetails(EmpDetails eDatils);
+        [OperationContract]
+        DataSet FetchUpdatedRecords(EmpDetails eDatils);
+        [OperationContract]
+        string UpdateEmpDetails(EmpDetails eDatils);
+        [OperationContract]
+        bool DeleteEmpDetails(EmpDetails eDatils);
     }
 
 
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+
     [DataContract]
-    public class CompositeType
+    public class EmpDetails
     {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
+        int? eId;
+        string eName = string.Empty;
+        string eSalary = string.Empty;
+        string eDeptId = string.Empty;
+        string eDeptName = string.Empty;
         [DataMember]
-        public bool BoolValue
+        public int? Id
         {
-            get { return boolValue; }
-            set { boolValue = value; }
+            get
+            {
+                return eId;
+            }
+            set
+            {
+                eId = value;
+            }
         }
-
         [DataMember]
-        public string StringValue
+        public string Name
         {
-            get { return stringValue; }
-            set { stringValue = value; }
+            get
+            {
+                return eName;
+            }
+            set
+            {
+                eName = value;
+            }
+        }
+        [DataMember]
+        public string Salary
+        {
+            get
+            {
+                return eSalary;
+            }
+            set
+            {
+                eSalary = value;
+            }
+        }
+        [DataMember]
+        public string DeptId
+        {
+            get
+            {
+                return eDeptId;
+            }
+            set
+            {
+                eDeptId = value;
+            }
+        }
+        [DataMember]
+        public string DeptName
+        {
+            get
+            {
+                return eDeptName;
+            }
+            set
+            {
+                eDeptName = value;
+            }
         }
     }
-}
+} 
+
--- WCF_Crud/Service1.svc.cs
+++ WCF_Crud/Service1.svc.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -12,22 +14,100 @@ namespace WCF_Crud
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        SqlConnection con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString);
+        public string InsertEmpDetails(EmpDetails eDetails) //For Insert Purpose  
         {
-            return string.Format("You entered: {0}", value);
+            string Status;
+            SqlCommand cmd = new SqlCommand("USP_Emp_Insert", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Name", eDetails.Name);
+            cmd.Parameters.AddWithValue("@Salary", eDetails.Salary);
+            cmd.Parameters.AddWithValue("@DeptId", eDetails.DeptId);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            int result = cmd.ExecuteNonQuery();
+            if (result == 1)
+            {
+                Status = eDetails.Name + " " + eDetails.Salary + " Is Registered Successfully";
+            }
+            else
+            {
+                Status = eDetails.Name + " " + eDetails.Salary + " could not be registered";
+            }
+            con.Close();
+            return Status;
         }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public DataSet GetEmpDetails(EmpDetails eDetails) //For Details Purpose  
         {
-            if (composite == null)
+            SqlCommand cmd = new SqlCommand("Get_AllEmployees", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Id", eDetails.Id);
+            if (con.State == ConnectionState.Closed)
             {
-                throw new ArgumentNullException("composite");
+                con.Open();
             }
-            if (composite.BoolValue)
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return ds;
+        }
+        public DataSet FetchUpdatedRecords(EmpDetails eDetails) //For update details Purpose  
+        {
+            SqlCommand cmd = new SqlCommand("Get_AllEmployees", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Id", eDetails.Id);
+            if (con.State == ConnectionState.Closed)
             {
-                composite.StringValue += "Suffix";
+                con.Open();
             }
-            return composite;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return ds;
+        }
+        public string UpdateEmpDetails(EmpDetails eDetails) //For Update Purpose  
+        {
+            string Status;
+            SqlCommand cmd = new SqlCommand("USP_Emp_Update", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Id", eDetails.Id);
+            cmd.Parameters.AddWithValue("@Name", eDetails.Name);
+            cmd.Parameters.AddWithValue("@Salary", eDetails.Salary);
+            cmd.Parameters.AddWithValue("@DeptId", eDetails.DeptId);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            int result = cmd.ExecuteNonQuery();
+            if (result == 1)
+            {
+                Status = "Record Is Updated successfully";
+            }
+            else
+            {
+                Status = "Record could not be updated";
+            }
+            con.Close();
+            return Status;
+        }
+        public bool DeleteEmpDetails(EmpDetails eDetails) //For Delete Purpose  
+        {
+            SqlCommand cmd = new SqlCommand("USP_Emp_Delete", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Id", eDetails.Id);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return true;
         }
     }
 }
